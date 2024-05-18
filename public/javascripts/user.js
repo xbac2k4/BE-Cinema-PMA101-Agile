ce28c3079ff6747650cb67d01d6179d0091a54be
xbac2k4 <162277991+xbac2k4@users.noreply.github.com>
2024-05-18 21:56:58 +0700

    as

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -2,6 +2,8 @@ const url = 'http://localhost:3000/api/v1/user'
 
 let tbody = document.querySelector('tbody')
 let page = document.getElementById('page')
+let dialog = document.getElementById('bgr-dialog-chitiet')
+let dialogbody = document.getElementById('dialog-chitiet')
 let preloader = document.getElementById('preloader')
 let pageNumber = document.getElementById('page-number')
 let tang = document.getElementById('ic-tang')
@@ -9,6 +11,9 @@ let giam = document.getElementById('ic-giam')
 
 let numberPage = 1;
 let totalPages;
+
+console.log(dialog);      // Should not be null
+console.log(dialogbody);  // Should not be null
 
 const fetchAPI_Page = (currentPage) => {
     fetch(`${url}/get-user-by-page?page=${currentPage}&limit=5`)
@@ -19,26 +24,30 @@ const fetchAPI_Page = (currentPage) => {
             return response.json(); // Đọc nội dung của phản hồi
         })
         .then(data => {
-            console.log("data ok: " + data.data.users);
+            // console.log("data ok: " + data.data.users);
             let html = data.data.users.map(items => {
                 return /*html*/` 
                 <tr>
-                    <td><p style="
-                    width: 70px;
-                    color: red;   
-                    white-space: nowrap; 
-                    overflow: hidden;
-                    text-overflow: ellipsis;">${items._id}</p></td>
+                    <td>
+                        <p style="
+                            width: 70px;
+                            color: red;   
+                            white-space: nowrap; 
+                            overflow: hidden;
+                            text-overflow: ellipsis;">${items._id}</p>
+                    </td>
                     <td>
                         <img style="width:30px;height:30px,object-fit:cover" src="${items.avatar}"/>
-                    </td>
-                    
+                    </td>               
                     <td>${items.username}</td>
                     <td>${items.sex=0?'Nam': 'Nữ'}</td>
                     <td>${items.email}</td>
                     <td>${items.phoneNumber}</td>
+                    <td style="gap: 20px; font-size: 20px" class="d-flex justify-content-end">
+                        <i onclick="BtnChiTiet()" class="bi bi-eye"></i> 
+                        <i class="bi bi-pen"></i> 
+                        <i class="bi bi-trash3"></i>
                     </td>
-                    <td style="gap: 20px; font-size: 20px" class="d-flex justify-content-end"><i onclick="BtnChiTiet()" class="bi bi-eye"></i> <i class="bi bi-pen"></i> <i class="bi bi-trash3"></i></td>
                 </tr>
             `;
             }).join('');
@@ -52,11 +61,17 @@ const fetchAPI_Page = (currentPage) => {
             console.error('There was a itemsblem with the fetch operation:', error);
         });
 }
-{/* <button onclick="BtnXoa('${items._id}')" class="btn btn-outline-danger">Chi tiết</button> <button class="btn btn-outline-info" onclick="BtnSua('${items._id}','${items.image}','${items.name}')">Sửa</button> <button onclick="BtnXoa('${items._id}')" class="btn btn-outline-danger">Xóa</button> */ }
+
 fetchAPI_Page(numberPage);
 
-const BtnChiTiet = () => {
-    alert('Chức năng đang được phát triển');
+const BtnChiTiet = (items) => {
+    // alert(`Chức năng đang được phát triển ${name}`);
+    dialog.style.display = 'flex';
+    console.log(items);
+}
+
+const closeDialogChiTiet = () => {
+    dialog.style.display = 'none';
 }
 
 tang.addEventListener('click', event => {
